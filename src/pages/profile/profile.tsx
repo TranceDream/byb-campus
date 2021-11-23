6516c1154c9ade84f1203e1c6da5d2334d7c81ea
wyh <sjwyh2000@163.com>
2021-11-23 14:36:06 +0800

    test1 for static page

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,16 +1,38 @@
-import { Flex, Text } from 'antd-mobile'
+import { Flex, Text, View } from 'antd-mobile'
 import { Component } from 'react'
+import { Image } from '@tarojs/components'
+
+import userIcon from '../../images/mg.png'
 
 import styles from './profile.module.sass'
 
 export default class Profile extends Component {
+
 	render() {
 		return (<>
             <Flex className={styles.wrapper}>
-                <Text>
-                    Profile here
-                </Text>
+                <div className={styles.user}>
+                    <Image className={styles.pic} src={userIcon} />
+                    <Text className={styles.userName}>
+                        用户名
+                    </Text> 
+                </div>
+                
+                <Text className={styles.box1}>
+                    Profile
+                </Text>    
+                
+                <div className={styles.divLine}></div>
+
+               <Text className={styles.box1}>
+                    Profile
+                </Text>    
+
+                <div className={styles.divLine}></div> 
+
             </Flex>
+                
+            
         </>)
 	}
 }
