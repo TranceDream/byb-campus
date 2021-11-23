39585b597735d5b95143e8de8c1e70784de6385c
wyh <sjwyh2000@163.com>
2021-11-23 23:50:43 +0800

    foundation of profile

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,12 +1,25 @@
-import { Flex, Text, View } from 'antd-mobile'
+import { Flex, Text } from 'antd-mobile'
 import { Component } from 'react'
 import { Image } from '@tarojs/components'
+import Taro from '@tarojs/taro'
 
-import userIcon from '../../images/mg.png'
+import userIcon from '../../images/user.png'
 
 import styles from './profile.module.sass'
 
+
 export default class Profile extends Component {
+
+    toP1(){
+        Taro.navigateTo({
+            url:'../edit-profile/edit-profile'
+        })
+    }
+    toP2(){
+        Taro.navigateTo({
+            url:'../profile2/profile2'
+        })
+    }
 
 	render() {
 		return (<>
@@ -15,24 +28,26 @@ export default class Profile extends Component {
                     <Image className={styles.pic} src={userIcon} />
                     <Text className={styles.userName}>
                         用户名
-                    </Text> 
+                    </Text>
+
                 </div>
                 
-                <Text className={styles.box1}>
-                    Profile
-                </Text>    
-                
+                <div className={styles.divLine0}></div>
+ 
+                <Text className={styles.box1} onClick={this.toP1}>
+                    Edit your profile
+                </Text>
+
                 <div className={styles.divLine}></div>
 
-               <Text className={styles.box1}>
-                    Profile
-                </Text>    
+                <Text className={styles.box1} onClick={this.toP2}>
+                    Profile2
+                </Text>
 
                 <div className={styles.divLine}></div> 
 
             </Flex>
                 
-            
         </>)
 	}
 }
